40c1bf8ba23923e2e06fd1e504980718794de466
Weijia Wang <starkwang@126.com>
2019-10-23 15:28:29 +0800

    加入e2e测试用例

--- test/e2e/cases/index.test.js
+++ test/e2e/cases/index.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable max-nested-callbacks */
+
 describe('数据库', () => {
   it('DB查询', async () => {
     const page = global.page;
@@ -10,6 +11,37 @@ describe('数据库', () => {
     });
 
     expect(result.data.length > 0).toBeTruthy();
+  });
+
+  it('插入Point', async () => {
+    const page = global.page;
+    let result = await page.evaluate(() => {
+      const db = app.database();
+      return db.collection('test').add({
+        point: new db.Geo.Point(-180, -1)
+      });
+    });
+
+    const { id } = result;
+    expect(id).toBeDefined();
+
+    result = await page.evaluate(() => {
+      const db = app.database();
+      return db.collection('test').where({
+        point: new db.Geo.Point(-180, -1)
+      }).get();
+    });
+
+    expect(result.data.length > 0).toBeTruthy();
+
+    result = await page.evaluate((id) => {
+      const db = app.database();
+      return db.collection('test').where({
+        _id: id
+      }).remove();
+    }, id);
+
+    expect(result.deleted).toEqual(1);
   });
 });
 
--- test/e2e/setup.js
+++ test/e2e/setup.js
@@ -6,6 +6,8 @@ const tcbjs = fs.readFileSync('./tcbjs/e2e/tcb.js', 'utf-8');
 
 const browser = global.__BROWSER__;
 
+jest.setTimeout(20000); // 20 second
+
 async function setup() {
   const page = await browser.newPage();
   global.page = page;
